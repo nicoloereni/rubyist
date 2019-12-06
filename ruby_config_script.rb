9fb48c15e5d7f084c9ca7931853ca0d97651a93b
Nicolo Ereni <nicolo.ereni@nozominetworks.com>
2019-12-06 16:39:51 +0100

    better viz

--- ruby_config_script.rb
+++ ruby_config_script.rb
@@ -1,7 +1,7 @@
 class RubyConfig
   def self.all
     %w(rubylibdir bindir archdir sitedir vendordir sitelibdir sitearchdir)
-        .each { |c| p RbConfig::CONFIG[c]}
+        .each { |c| p "#{c}: #{RbConfig::CONFIG[c]}"}
   end
 end
 RubyConfig.all
